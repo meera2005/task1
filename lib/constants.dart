54f621b8f77347f0742bf98c4bfa4dbd72862aee
meera2005 <meera200205@gmail.com>
2022-01-29 17:13:25 +0530

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+const FOOD_DATA = [
+  {
+    "name":"TECHNICAL",
+    "image":"technical.png"
+  },
+  {
+    "name":"ARTS",
+    "image":"arts.png"
+  },
+  {
+    "name":"VIDEO GAMES",
+    "image":"games.png"
+  },
+  {
+    "name":"HACKATHONS",
+    "image":"hackathons.png"
+  }
+
+];
+
